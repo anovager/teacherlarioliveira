a4e2758ed1453b99120eb910b45c75e382935715
Anovager <163933716+anovager@users.noreply.github.com>
2025-03-14 17:36:16 -0300

    add: primeiro commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.section', { 
+    duration: 800, 
+    origin: 'bottom', 
+    distance: '50px',
+    easing: 'ease-in-out',
+    reset: true // Se quiser que o efeito aconteça sempre que a seção reaparecer
+});
+document.getElementById("year").textContent = new Date().getFullYear();
