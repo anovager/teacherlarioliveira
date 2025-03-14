5ba02a4d0c2d4b1cb4b05783f3200873f53aba9d
Anovager <163933716+anovager@users.noreply.github.com>
2025-03-14 17:51:01 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@ ScrollReveal().reveal('.section', {
     origin: 'bottom', 
     distance: '50px',
     easing: 'ease-in-out',
-    reset: true // Se quiser que o efeito aconteça sempre que a seção reaparecer
+    reset: false // Se quiser que o efeito aconteça sempre que a seção reaparecer
 });
 document.getElementById("year").textContent = new Date().getFullYear();
